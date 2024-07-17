25cb654c482a73c4839c736833b487e08287c2c2
kid2ee2e32 <165047333+kid2ee2e32@users.noreply.github.com>
2024-07-17 18:37:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+    content="Hacker Club NFT collection. A collection with multiple hackers with valueable properties.">
+    <link rel="canonical" href="https://www.hackerhubnft.com" />
+
+    <title>Hacker Hub Nft </title>
+
+    <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Favicon Icon -->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+    <!-- Main Style -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+</head>
+<body>
+      <!-- Site Preloader -->
+  <div id="site-preloader">
+    <div class="spinner"></div>
+  </div>
+
+    <!------------- HEADER START ------------->
+    <header>
+        <div class="wrapper">
+            <div class="logo">
+                <a href="/">
+                    <img src="assets/img/logo.png" alt="Logo" height="110" width="auto">
+                </a>
+            </div>
+
+            <!------------- HEADER MOBILE START ------------->
+            <div id="toggle">
+                <img src="assets/img/menu.png" alt="navbar">
+            </div>
+
+            <nav class="navigation-mobile" id="toggle-content">
+
+                    <div class="navigation-points">
+                        <a href="https://opensea.io/collection/hackerhub" class="animated">B<span id="offset">uy</span></a>
+                        <a class="goto" href="#projects">Projects</a>
+                        <a  class="goto" href="#about">About</a>
+                    </div>
+    
+                    <hr>
+
+                    <div class="social-media">
+                        <a href="https://opensea.io/collection/hackerhub"><img src="assets/img/opensea.png" alt="opensea" height="18"></a>
+                        <a href="https://www.instagram.com/hackerhub_nft/"><img src="assets/img/instagram.png" alt="instagram" height="18"></a>
+                        <a href=""><img src="assets/img/twitter.png" alt="twitter" height="18"></a>
+                        <a href="https://discord.com/invite/wa6DNyyYFJ"><img src="assets/img/discord.png" alt="discord" height="18"></a>
+                    </div>
+            </nav>
+            <!------------- HEADER MOBILE END------------->
+
+            <nav class="navigation">
+                <div class="navigation-points">
+                    <a href="https://opensea.io/collection/hackerhub" class="animated">B<span id="offset">uy</span></a>
+                    <a class="goto" href="#projects">Projects</a>
+                    <a class="goto" href="#about">About</a>
+                </div>
+
+                <div class="social-media">
+                    <a href="https://opensea.io/collection/hackerhub"><img src="assets/img/opensea.png" alt="opensea" height="18"></a>
+                    <a href="https://www.instagram.com/hackerhubnft/"><img src="assets/img/instagram.png" alt="instagram" height="18"></a>
+                    <a href=""><img src="assets/img/twitter.png" alt="twitter" height="18"></a>
+                    <a href="https://discord.com/invite/wa6DNyyYFJ"><img src="assets/img/discord.png" alt="discord" height="18"></a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!------------- HEADER END ------------->
+
+        <!------------- MAIN START ------------->
+    <main>
+            <!------------- MAIN PREVIEW START ------------->
+        <section>
+            <div class="preview">
+                <img src="assets/img/preview.png" alt="preview">
+                <!------                <div class="image-title">
+                    <h1>One of the coolest collectable<br> Nft 
+                        collection
+                    </h1>
+                </div>------>
+                <div class="countdown">
+                <p id="countdown"></p>
+                </div>
+            </div>
+        </section>
+            <!------------- MAIN PREVIEW END ------------->
+
+
+            <!------------- HEADER HACKERHUB START ------------->
+        <section>
+            <div class="container">
+                <div class="text-title">
+                    <h3>HACKER HUB</h3>
+                </div>
+
+                <div class="text">
+                    <p>
+                        Hacker Hub is a Nft collection of 1000 ever minted and uniquely generated
+                         hacker pixel artworks. Each of these masterpieces who are living on the Polygon blockchain have its own unique personality with
+                        extremely cool properties and a awesome look. You will be able to sell, buy and use each of 
+                        these masterpieces. Over time,
+                         more and more NFTs will be added but there will be some really unique ones which will be released in the last drops. <span class="marker">
+                            To see the release dates, please read below.
+                         </span>
+
+                    </p>
+                </div>
+                <hr>
+            </div>
+        </section>
+            <!------------- MAIN HACKERHUB END ------------->
+
+            <!------------- MAIN COLECTION START ------------->
+        <section>
+            <div class="container" id="projects">
+                <div class="text-title">
+                    <h3>COLLECTION</h3>
+                </div>
+
+                <div class="collection">
+                    <div class="collection-image" id="collection">
+                        <div class="image"><a href="#"><img src="assets/img/nfts/<?= random_int(0,10) ?>.png" alt="Nft" ></a><div class="image-text"><p>???</p><h4>Evil Hacker</h4></div></div>
+                        <div class="image"><a href="#"><img src="assets/img/nfts/<?= random_int(0,10) ?>.png" alt="Nft" ></a><div class="image-text"><p>???</p><h4>Young Hacker </h4></div></div>
+                        <div class="image"><a href="#"><img src="assets/img/nfts/<?= random_int(0,10) ?>.png" alt="Nft" ></a><div class="image-text"><p>???</p><h4>Friendly Hacker </h4></div></div>
+                        <div class="image"><a href="#"><img src="assets/img/nfts/<?= random_int(0,10) ?>.png" alt="Nft" ></a><div class="image-text"><p>???</p><h4>Vigilante Hacker </h4></div></div>
+                        <div class="image"><a href="#"><img src="assets/img/nfts/<?= random_int(0,10) ?>.png" alt="Nft" ></a><div class="image-text"><p>???</p><h4>Evil Hacker </h4></div></div>
+                        <div class="image"><a href="#"><img src="assets/img/nfts/<?= random_int(0,10) ?>.png" alt="Nft" ></a><div class="image-text"><p>???</p><h4>Neutral Hacker </h4></div></div>
+                    </div>
+                </div>
+
+                <div class="see-more">
+                    <a href="https://opensea.io/hackerhub" id="more">See more...</a>
+                </div>
+
+                <hr>
+            </div>
+        </section>
+            <!------------- MAIN COLLECTION END ------------->
+            <!------------- MAIN INFORMATION HACKER START ------------->
+        <section>
+            <div class="container">
+                <div class="text-title">
+                    <h3>HACKERS</h3>
+                </div>
+
+                <div class="text">
+                    <p>
+                        Who doesn't think hackers are cool? Hackers are one of the coolest and most popular characters on the internet
+                    </p>
+
+                    <ul>
+                        <li><span class="marker">Friendly Hacker:</span> Helps other people</li>
+                        <li><span class="marker">Neutral Hacker:</span> Not malicious, but not always ethical</li>
+                        <li><span class="marker">Young Hacker:</span> Malicious, but for his personal purposes</li>
+                        <li><span class="marker">Vigilante Hacker:</span> Hacking against the state</li>
+                        <li><span class="marker">Evil Hacker:</span> Extremely dangerous and talented</li>
+    
+
+                    </ul>
+                </div>
+
+                <hr>
+            </div>
+        </section>
+            <!------------- MAIN INFORMATION HACKER END ------------->
+
+            <section>
+                <div class="container">
+                    <div class="text-title">
+                        <h3>DROP TIME</h3>
+                    </div>
+    
+                    <div class="text">
+                        <p>
+                            Each of our hacker pixel masterpieces will be released separately and available for purchase. 
+                            It is divided into 3 different releases. 
+                            The last 2 releases will include some extremely valueable and rare NFTS.
+                            The first on<span class="marker"> 01/04/22 100/1000 Nfts</span>.
+                            The second on <span class="marker">10/04/22 500/1000 Nfts</span>.
+                            The last on <span class="marker">20/04/22 1000/1000 Nfts</span>. To get notified follow our social media.
+                        </p>
+                    </div>
+    
+                    <hr>
+                </div>
+            </section>
+
+
+
+            <!------------- MAIN GOALS START ------------->
+                <section>
+                    <div class="goals">
+                    <div class="text-title">
+                        <h3>ROADMAP</h3>
+                    </div>
+    
+                    <div class="goals-wrapper">
+                        <div class="goals-start">
+                            <div class="goal-title">
+                                <h3><span class="marker">1. Build Community</span></h3>
+                            </div>
+        
+                            <div class="goals-text">
+                                <p>Before we can start listing all of our NFTS, we first will have to build a community which will support us</p>
+                            </div>
+                        </div>
+        
+                        <div class="goals-transition"><div class="circle"></div><div class="circle"></div><img src="assets/img/transition.png" alt="transition"><div class="circle"></div><div class="circle"></div></div>
+        
+                        <div class="goals-medium">
+                            <div class="goal-title">
+                                <h3><span class="marker">2. Drop NFTS</span></h3>
+                            </div>
+        
+                            <div class="goals-text">
+                                <p>After we have built our community we will focus on listing our NFTS and dropping some awesome events </p>
+                            </div>
+                        </div>
+        
+        
+                        <div class="goals-transition"><div class="circle"></div><div class="circle"></div><img src="assets/img/transition2.png" alt="transition"><div class="circle"></div><div class="circle"></div></div>
+        
+                        <div class="goals-end">
+                            <div class="goal-title">
+                                <h3><span class="marker">3. Special Events</span></h3>
+                            </div>
+        
+                            <div class="goals-text">
+                                <p>At the end when everything was a success we will drop some awesome events like adding extra NFTS for those who joined in the beginning or even create a Hakcer Video Game where you will be able to fight with your collected Hackers !</p>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <hr>
+
+                </div>
+            </section>
+                <!------------- MAIN GOALS END ------------->
+        <section>
+            <div class="container" id="about">
+                <div class="text-title">
+                    <h3>ABOUT US</h3>
+                </div>
+
+                <div class="about">
+                    <div class="about-profile">
+
+                        <div class="profile">
+                            <div class="about-profile-image"><a href="#"><img src="assets/img/white-trans.png" alt="Tobias"></a> </div><div class="about-text"><h3>Rochdi Adel</h3><p>I'm Rochdi i'm 20 years old, a medical student and a professional designer</p></div>
+                        </div>
+
+                        <div class="profile">
+                            <div class="about-profile-image"><a href="#"><img src="assets/img/white-trans.png" alt="Tobias"></a> </div><div class="about-text"><h3>Tobias Vasconcelos</h3><p>I'm Tobi and I'm 16 years old. I am the founder and inventor of this collection and the developer of this site</p></div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+            <!------------- MAIN ABOUT END ------------->
+    </main>
+        <!------------- MAIN END ------------->
+
+
+            <!------------- FOOTER START ------------->
+
+    <footer>
+        <!------------- FOOTER WALLET START ------------->
+        <div class="wallet">
+            <div class="wallet-middle">
+                <p>Wallet:</p>
+                <p>0xa6675d96B2512d8CD0102943Fd2a86C169034276</p>
+            </div>
+        </div>
+        <!------------- FOOTER WALLET END ------------->
+
+        <div class="wrapper">
+            <div class="wrapper-left">
+                <div class="footer-logo">
+                   <a href="/"><img src="assets/img/logo.png" alt="Logo" height="180" width="auto"></a>
+                </div>
+            </div>
+
+            <div class="newsletter">
+                <form  method="POST" action="">
+                    <div class="input">
+                        <input type="email" name="email" maxlength="30" placeholder="E-Mail" value="">
+                        <button type="submit" name="submit" disabled><img src="assets/img/submit.png" alt="submit" width="auto" height="20"></button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="wrapper-right">
+                <div class="footer-social-media">
+                    <a href="https://opensea.io/collection/hackerhub"><img src="assets/img/opensea.png" alt="opensea" height="18"></a>
+                    <a href="https://www.instagram.com/hackerhubnft/"><img src="assets/img/instagram.png" alt="instagram" height="18"></a>
+                    <a href=""><img src="assets/img/twitter.png" alt="twitter" height="18"></a>
+                    <a href="https://discord.com/invite/wa6DNyyYFJ"><img src="assets/img/discord.png" alt="discord" height="18"></a>
+                </div>
+
+                <div class="term-text">
+                    <p>&copy;2022 HH</p>
+                    <a href="policy">Hacker Hub Terms</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!------------- FOOTER END ------------->
+
+
+    <!-- jQuery CDN -->
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js"
+  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
